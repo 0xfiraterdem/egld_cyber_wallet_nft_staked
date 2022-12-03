2f5c066363bcd704433512d11d1fe2721e26a8ff
firaterdm <48517359+firaterdm@users.noreply.github.com>
2022-12-03 13:20:44 +0300

    Update main.py
    Cüzdanların kaç seti olduğu eklendi

--- main.py
+++ main.py
@@ -31,25 +31,49 @@ for wallet in url:
         except:
             balance_unbonding=[]
 
-        z=balance_stake
         balance_stake.extend(balance_unbonding)
         balance_stake=list(set(balance_stake))
-        if len(balance_stake)==14:
-            snapshot.append(wallet['sender'])
+        for indx in range(0, len(balance_stake)):
+            balance_stake[indx] = base64.b64decode(balance_stake[indx]).hex()
+        if wallet['sender'] in wallets:
+            balance_stake.extend(wallet_balance_nft.wallet_balance(wallet['sender']))
+            balance_stake = list(set(balance_stake))
+            if len(balance_stake)==14:
+                balance =[]
+                for elmn in balance_stake:
+                    data3 = requests.post(post_url, json={
+                        "scAddress": "erd1qqqqqqqqqqqqqpgqsu2vxxx5l3tjgcnjl6mftlz5dtz5cp5s398syqw3gz",
+                        "funcName": "getUserStakedAmountPerNonce", "args": [f"{hex_wallet}", f"{elmn}"]})
+                    veri1 = int(base64.b64decode(data3.json()['returnData'][0]).hex(), 16)
+                    veri2 = wallet_balance_nft.wallet_per_nft_balance(wallet['sender'], elmn)
+                    if veri2 is not None:
+                        veri = veri1+int(veri2)
+                    else:
+                        veri = veri1
+                    balance.append(veri)
+                snapshot.append({"address": f"{wallet['sender']}", "set": f"{min(balance)}"})
         else:
-            if wallet['sender'] in wallets:
-                for indx in range(0,len(balance_stake)):
-                    balance_stake[indx]=base64.b64decode(balance_stake[indx]).hex()
-                balance_stake.extend(wallet_balance_nft.wallet_balance(wallet['sender']))
-                balance_stake = list(set(balance_stake))
-                if len(balance_stake)==14:
-                    snapshot.append(balance_stake)
+            if len(balance_stake) == 14:
+                balance = []
+
+                for elmn in balance_stake:
+                    data3 = requests.post(post_url,json={
+                                              "scAddress": "erd1qqqqqqqqqqqqqpgqsu2vxxx5l3tjgcnjl6mftlz5dtz5cp5s398syqw3gz",
+                                              "funcName": "getUserStakedAmountPerNonce", "args": [f"{hex_wallet}",f"{elmn}"]})
+                    balance.append(int(base64.b64decode(data3.json()['returnData'][0]).hex(),16))
+                snapshot.append({"address" : f"{wallet['sender']}", "set" : f"{min(balance)}"})
         time.sleep(0.6)
 
 if len(wallet_balance_nft.wallets_14nfts())!=0:
+    balance=[]
     for addr in wallet_balance_nft.wallets_14nfts():
-        if addr not in snapshot:
-            snapshot.append(addr)
+        for i in snapshot:
+            if addr not in i['address']:
+                for i in range(0,14):
+                    veri = wallet_balance_nft.wallet_per_nft_balance(addr, i)
+                    balance.append(veri)
+                snapshot.append({"address" : f"{addr}", "set" : f"{min(balance)}"})
+print(len(snapshot))
 ss= open("snapshot.txt", "w")
 ss.write(f'{snapshot}')
 ss= open("snapshot.txt", "r")
